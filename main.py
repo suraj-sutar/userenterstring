1405a791ea6213346165ee3be5db3ba446489f6f
Suraj sutar <66626903+suraj-sutar@users.noreply.github.com>
2022-12-11 05:50:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# name = "kashinath"
+# your_name = input("Enter your name: ")
+
+# print(f"hello {name}, My name is {your_name}...")
+
+# for example you want to accept the input from user that is age and print the input as "you have lived for 36 months in company" how can you print this
+
+# age = input("Pleace enter your age:")
+
+# print(f"you have lived for {age * 12} months ")
+# they print 3 in 12 times that is not our answer
+# input finction accept input from user that is in srting formate so this string formate conver the srting into integer then you can print the your accepted output
+
+# age = input("Enter your age: ")
+# age_as_num = int(age)
+# print(f"you have lived for {age_as_num * 12} months")
+
+# another formate
+
+# age = int(input("Enter your age :"))
+# print(f"You have for lived {age * 12} months")
+
+# another Formate
+# age = int(input("Enter ypur age : "))
+# months = age * 12
+# print(f"you have lived for {months} months.")
+
+x = '56'
+z = int(x) * 2
+
+print(z)
+
+
